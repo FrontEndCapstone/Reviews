dcb375428016e42af9284c45bf6bf29b7fe74518
Daniel Sockwell <dsockwell@gmail.com>
2018-10-26 15:27:58 -0400

    Set up basic reviews table in postgreSQL

--- server/db/db.config.js
+++ server/db/db.config.js
@@ -1,69 +1,50 @@
 const { Pool } = require('pg');
 const faker = require('faker');
 
-// pools will use environment variables
-// for connection information
-const pool = new Pool({
-  database: 'trailblazers-reviews',
-});
+const pool = new Pool({ database: 'trailblazers-reviews' });
 
-pool.query('DROP TABLE reviews', () => {
-  pool.query(
-    `CREATE TABLE reviews (
-      id int GENERATED always AS identity,
-      review_id int,
-      rating int,
-      reviewer varchar(255),
-      title varchar(255),
-      body text,
-      would_recommend boolean,
-      helpful_yes int,
-      helpful_no int,
-      PRIMARY KEY (id)
-    );`,
-    () => {
-      for (let i = 1; i <= 100; i++) {
-        if (Math.random() < 0.9) {
-          i--;
-        }
-        const query = {
-          name: 'fetch-user',
-          text: `
-        INSERT INTO reviews (
-          review_id,
-          rating,
-          reviewer,
-          title,
-          body,
-          would_recommend,
-          helpful_yes,
-          helpful_no
-        ) VALUES (
-          $1,
-          $2,
-          $3,
-          $4,
-          $5,
-          TRUE,
-          47,
-          0
-        )
-        `,
-          values: [
-            i,
-            1,
-            faker.internet.userName(),
-            faker.lorem.sentence(),
-            faker.lorem.paragraph(5),
-          ],
-        };
+const createQueryStr = `
+  CREATE TABLE reviews (
+    id int GENERATED always AS identity, review_id int, rating int,
+    reviewer varchar(255), title varchar(255), body text,
+    recommend boolean, helpful int, unhelpful int,
+    PRIMARY KEY (id)
+  );`;
 
-        pool.query(query, () => {
-          if (i === 100) {
-            pool.end();
-          }
-        });
+pool
+  .query('DROP TABLE reviews')
+  .then(() => {
+    pool.query(createQueryStr);
+  })
+  .then(() => {
+    for (let i = 1; i <= 100; i++) {
+      if (Math.random() < 0.9) {
+        i--;
       }
-    },
-  );
-});
+      const insertQuery = {
+        text: `
+INSERT INTO reviews (
+  review_id, rating, reviewer, title, body, recommend, helpful, unhelpful
+) VALUES (
+     $1,       $2,      $3,      $4,   $5,    $6,        $7,       $8
+)
+`,
+        values: [
+          i,
+          faker.random.number({ max: 3, min: 1 }) +
+            faker.random.number({ max: 2, min: 0 }),
+          faker.internet.userName(),
+          faker.lorem.sentence(),
+          faker.lorem.paragraph(5),
+          faker.random.boolean(),
+          faker.random.number(47),
+          faker.random.number(22),
+        ],
+      };
+      pool.query(insertQuery.text, insertQuery.values, () => {
+        if (i === 100) {
+          pool.end();
+        }
+      });
+    }
+  });
