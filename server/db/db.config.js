e108011fa184d5574525215d59e011a722a8f959
Daniel Sockwell <dsockwell@gmail.com>
2018-10-26 17:30:04 -0400

    Refactor seed script and add basic integration tests

--- server/db/db.config.js
+++ server/db/db.config.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 const faker = require('faker');
 
-const pool = new Pool({ database: 'trailblazers-reviews' });
+const db = new Pool({ database: 'trailblazers-reviews' });
 
 const createQueryStr = `
   CREATE TABLE reviews (
@@ -11,40 +11,51 @@ const createQueryStr = `
     PRIMARY KEY (id)
   );`;
 
-pool
-  .query('DROP TABLE reviews')
-  .then(() => {
-    pool.query(createQueryStr);
-  })
-  .then(() => {
-    for (let i = 1; i <= 100; i++) {
-      if (Math.random() < 0.9) {
-        i--;
-      }
-      const insertQuery = {
-        text: `
+const initReviewsTable = () => {
+  db.connect((connectionErr, client, done) => {
+    client.query('BEGIN', err => {
+      client.query('DROP TABLE reviews');
+      client.query(createQueryStr, (err, res) => {
+        for (let i = 1; i < 100; i++) {
+          if (Math.random() < 0.9 && i < 100) {
+            i--;
+          }
+          const insertQuery = {
+            text: `
 INSERT INTO reviews (
   review_id, rating, reviewer, title, body, recommend, helpful, unhelpful
 ) VALUES (
      $1,       $2,      $3,      $4,   $5,    $6,        $7,       $8
-)
-`,
-        values: [
-          i,
-          faker.random.number({ max: 3, min: 1 }) +
-            faker.random.number({ max: 2, min: 0 }),
-          faker.internet.userName(),
-          faker.lorem.sentence(),
-          faker.lorem.paragraph(5),
-          faker.random.boolean(),
-          faker.random.number(47),
-          faker.random.number(22),
-        ],
-      };
-      pool.query(insertQuery.text, insertQuery.values, () => {
-        if (i === 100) {
-          pool.end();
+)`,
+            values: [
+              i,
+              faker.random.number({ max: 3, min: 1 }) +
+                faker.random.number({ max: 2, min: 0 }),
+              faker.internet.userName(),
+              faker.lorem.sentence(),
+              faker.lorem.paragraph(5),
+              faker.random.boolean(),
+              faker.random.number(47),
+              faker.random.number(22),
+            ],
+          };
+          client.query(insertQuery.text, insertQuery.values, (err, res) => {
+            // console.log('i: ', i);
+            if (i === 99) {
+              client.query('COMMIT', err => {
+                if (err) {
+                  console.error('Error committing transaction', err.stack);
+                }
+                done();
+              });
+            }
+          });
         }
       });
-    }
+    });
   });
+};
+
+initReviewsTable();
+
+module.exports = initReviewsTable;
